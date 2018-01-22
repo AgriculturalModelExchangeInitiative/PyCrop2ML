8bebe70e8a874f12f761f9fee2efc9be6cd4ac66
cyrillemidingoyi <34443570+cyrillemidingoyi@users.noreply.github.com>
2018-01-22 13:00:59 +0100

    Update essai.py

--- test/essai.py
+++ test/essai.py
@@ -1,16 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 21 13:59:40 2018
 
-@author: midingoy
-"""
 
 from path import Path
-"""import parser as xx"""
-
 import pparse as pp
-
-
 data = Path('data')
 xmls = data.glob('*.xml')
 
