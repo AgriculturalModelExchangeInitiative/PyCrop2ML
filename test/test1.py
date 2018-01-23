6e420486b9225ec6e634a2ac175cb95805855f43
pradal <christophe.pradal@inria.fr>
2018-01-23 19:09:26 +0100

    Add real test

--- test/test1.py
+++ test/test1.py
@@ -13,8 +13,37 @@ xmls = data.glob('*.xml')
 ##############################################################################
 # Test on Example
 
-def test1():
+def example():
     fn = data.glob('Example*.xml')[0]
 
     models = pparse.parse(fn)
     return models
+
+def test1():
+    models = example()
+    assert len(models) == 1
+
+    model = models[0]
+    assert model.algorithm
+
+    lines = [l for l in model.algorithm.split('\n') if l.strip()]
+    assert len(lines) == 3
+
+    assert len(model.inputs) == 10
+    # TODO: check the different inputs (name at least)
+
+    assert len(model.outputs) == 1
+    # TODO Check the output
+
+    # check Description:
+    #TODO
+
+    # Check ParameterSets
+    assert len(model.parametersets) == 5
+    v = ['soil', 'sorghum', 'wheat', 'crop', 'wheat.cv1']
+    for k in v:
+        assert k in model.parametersets
+
+    # Check each parameterset: TODO
+
+    # Check tests: TODO
