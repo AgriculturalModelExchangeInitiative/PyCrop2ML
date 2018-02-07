2e363f91f71e7b6f56b955f80fefc1be78ad0a77
cyrillemidingoyi <ahmedmidingoyi@yahoo.fr>
2018-02-07 08:44:22 +0100

    improve test_generation

--- test/myprog.py
+++ test/myprog.py
@@ -65,50 +65,80 @@ for test in (t1, t2):
             if filename not in file_tsets:
                 file_tsets[filename] = pparse.test_parser(_filename, m)
 
-        # Build the content of the test with the run
-        # Generate the Python code
 
-    # Print the set of test parameters
-    test_codes = []
-    if test.name in m.tests:
-        test_runs = m.tests[test.name]
+# Build the content of the test with the run
+# Generate the Python code
 
-        # map the paramsets
-        params = {}
-        for pname in test.paramsets:
-            if pname not in psets:
-                print('Unknow parameter %s'%pname)
-            else:
-                params.update(psets[pname].params)
+m2p = render_python.Model2Package(models);
+m2p.run()
+code = m2p.code
+exec(code)
 
+for v_tests in m.tests.values():
+    
+    test_name = v_tests[0].name  # name of tests
+    test_runs = v_tests[0].run  # different run in the thest
+    test_paramsets = v_tests[0].paramsets  # name of paramsets
+    
+    # map the paramsets
+    params = {}
+    for pname in test_paramsets:
+        if pname not in psets:
+            print('Unknow parameter %s'%pname)
+        else:
+            params.update(psets[pname].params)
+    
+    for each_run in test_runs :
+        test_codes = []
+        
         # make a function that transforms a title into a function name
-        tname = test.name.replace(' ', '_')
+        tname = test_name.replace(' ', '_')
         tname = tname.replace('-', '_')
-        for tr in test_runs:
-            (run, inouts) = tr.items()[0]
-            ins = inouts['inputs']
-            outs = inouts['outputs']
+       
+               
+        (run, inouts) = each_run.items()[0]
+    
+        ins = inouts['inputs']
+        outs = inouts['outputs']
+        
+        code = '\n'
+        test_codes.append(code)
 
-            code = '\n'
+        code = "def test_%s_run%s():"%(tname, run)
+        test_codes.append(code)
+        code = "    params= %s("%model_name
+        test_codes.append(code)
+
+        run_param = params.copy()
+        run_param.update(ins)
+        
+        for k, v in run_param.iteritems():
+            code = "    %s = %s,"%(k,v)
             test_codes.append(code)
+        code = "     )"
+        test_codes.append(code)
 
-            code = "def test_%s_run%s():"%(tname, run)
-            test_codes.append(code)
-            code = "    params= %s("%model_name
-            test_codes.append(code)
+               
+        code = "\n"+"    print ("'"params : {}"'+".format(params)"')'
+        
+        test_codes.append(code)
+        
+        code = "    return params"
+        
+        test_codes.append(code)
 
-            run_param = params.copy()
-            run_param.update(ins)
-
-            for k, v in run_param.iteritems():
-                code = "    %s = %s,"%(k,v)
-                test_codes.append(code)
-            code = "     )"
-            test_codes.append(code)
-
-            # Get from the params the different values
-
-            # Then, assert that the values are equal to the outs.
-
-            the_code = '\n'.join(test_codes)
-            print the_code
+        the_code = '\n'.join(test_codes)
+        
+        exec the_code
+        
+        print the_code
+        
+         # Get from the params the different values
+        
+        func = 'test_%s_run%s()'%(tname, run)
+        exec func
+        
+        # Then, assert that the values are equal to the outs.
+        
+        assert func == float(outs.values()[0])
+        
