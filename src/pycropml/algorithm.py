c58873076a751c98a647e49dec6981740c12b5da
cyrillemidingoyi <ahmedmidingoyi@yahoo.fr>
2018-10-14 12:04:18 +0200

    add function and filename

--- src/pycropml/algorithm.py
+++ src/pycropml/algorithm.py
@@ -1,7 +1,9 @@
 class Algorithm(object):
     """ Algorithm """
  
-    def __init__(self, language, development, platform):
+    def __init__(self, language, development, platform, function=None, filename=None):
         self.language = language 
         self.platform = platform
+        self.function = function
+        self.filename = filename
         self.development = development
