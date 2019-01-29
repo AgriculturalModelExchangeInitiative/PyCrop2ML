cf683741220187b4d2dd61aa76b7dbd16cadb23a
cyrillemidingoyi <ahmedmidingoyi@yahoo.fr>
2019-01-29 13:57:17 +0100

    test for parsing cython code

---
+++ tests_transpiler/data/example1.pyx
@@ -0,0 +1,16 @@
+def diffusionlimitedevaporation(float deficitOnTopLayers,
+                                float soilDiffusionConstant):
+    
+    cdef float diffusionLimitedEvaporation 
+    if (deficitOnTopLayers / 1000.0 <= 0):
+        diffusionLimitedEvaporation = 8.3 * 1000
+    else:
+        if (deficitOnTopLayers / 1000 < 25):
+            diffusionLimitedEvaporation = (2 * soilDiffusionConstant * soilDiffusionConstant / (deficitOnTopLayers / 1000.0)) * 1000.0
+        else:
+            diffusionLimitedEvaporation = 0
+    return diffusionLimitedEvaporation
+
+
+
+      
