4ce6063ffe20f474f054815a2b504889590f02bd
cyrillemidingoyi <ahmedmidingoyi@yahoo.fr>
2019-02-01 10:24:14 +0100

    new Node class

---
+++ src/pycropml/transpiler/pseudo_tree.py
@@ -0,0 +1,21 @@
+import yaml
+class Node:
+    '''
+    The new Node generated with specific properties.
+    These properties are automatically set"
+    
+    Example: Node('local', name='l', pseudo_type="int") to represent a int variable declaration
+    '''
+    def __init__(self, type, **fields):
+        self.type = type
+        self.__dict__.update(fields) 
+        if 'pseudo_type' not in fields:
+            self.pseudo_type = 'Void'
+    
+    @property
+    def y(self):
+        result = yaml.dump(self)
+        return result.replace('!!python/object:pycropml.transpiler.pseudo_tree.', '')
+
+    def __eq__(self, a):
+        return all(getattr(self, f) == getattr(a, f, None) for f in self.__dict__)
