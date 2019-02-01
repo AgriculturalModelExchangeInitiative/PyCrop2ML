690b974cf89e4291d02717ab28956f44c776a99a
cyrillemidingoyi <ahmedmidingoyi@yahoo.fr>
2019-02-01 10:24:50 +0100

    diffrent specific rules of language for code generation

---
+++ src/pycropml/transpiler/rules/csharpRules.py
@@ -0,0 +1,22 @@
+csharpOP = {"and":"&&",
+            "or":"||",
+            "not": "!",
+            "<":"<",
+            ">":">",
+            "==":"==",
+            "+":"+",
+            "-":"-",
+            "*":"*",
+            "/":"/"
+        }
+
+types={
+       "int": "int",
+       "float":"double",
+       "bool":"bool",
+       "int":"int",
+       "list":"List",
+       "tuple":"Tuple",
+       "str":"string"       
+       }
+
