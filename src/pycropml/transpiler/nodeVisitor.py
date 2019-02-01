10904032972bf924dc0e5ff6633bb7431884185d
cyrillemidingoyi <ahmedmidingoyi@yahoo.fr>
2019-02-01 10:23:36 +0100

    node visitor for code generation

---
+++ src/pycropml/transpiler/nodeVisitor.py
@@ -0,0 +1,16 @@
+from pycropml.transpiler.pseudo_tree import Node
+class NodeVisitor:
+    '''
+    Define a method which browse the graph and call a methode constructed from the type
+    of each node of the graph
+    '''
+    def visit(self, node):
+        if isinstance(node, list):
+            for n in node:
+                self.visit(n)
+        if not isinstance(node, Node):
+            return node
+        elif hasattr(self, 'visit_%s' % node.type):
+            return getattr(self, 'visit_%s' % node.type)(node)
+        else:
+            raise NotImplementedError("no action for %s" % node.type)
