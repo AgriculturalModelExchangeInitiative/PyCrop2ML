eb412d8094b76fe7d9b93660d12dff5055742365
cyrillemidingoyi <ahmedmidingoyi@yahoo.fr>
2019-07-30 09:48:25 +0200

    some FST functions

---
+++ src/pycropml/transpiler/lib/pyx/FST_Functions.pyx
@@ -0,0 +1,62 @@
+# FSTFunctions
+def AFGEN1(int n, float xy[n], float t):
+    cdef float res;
+    cdef float x1, x2, y1, y2
+    cdef int i
+    res = float("nan")
+    if(n>1):
+        i = 0
+        while (i < n-1 and t >= xy[i]):
+            i = i + 2
+        if(i==0):
+            res = xy[1]
+        elif (i>n-2):
+            res = xy[i-1]
+        else:
+            x1 = xy[i-2]
+            x2 = xy[i]
+            y1 = xy[i-1]
+            y2 = xy[i+1]
+            res = (y2 - y1)/(x2 - x1) * (t-x1) + y1
+    return res
+
+def AFGEN2(int nx, int ny, float x[nx], float y[ny], float t):
+    cdef float res, x1, x2, y1, y2 
+    cdef int n, i
+    n = min(len(x),len(y))
+    res = float("nan")
+    if(n>0):
+        i = 0
+        while (i < n and t >= x[i]):
+            i = i+1
+        if(i==0):
+            res = y[0]
+        elif (i==n):
+            res = y[n-1]
+        else:
+            x1 = x[i-1]
+            x2 = x[i]
+            y1 = y[i-1]
+            y2 = y[i]
+            res = (y2 - y1)/(x2 - x1) * (t-x1) + y1
+    return res
+
+def AFGEN3(dict xy={0.0:0.0}, float t=1.5):
+    cdef float x[len(xy)] 
+    cdef float y[len(xy)]
+    cdef int i=0
+    cdef float tKey, res
+    cdef int l
+    l =len(xy)
+    for tKey in xy.keys():
+        x[i] = tKey;
+        y[i] = xy.get(tKey)
+        i = i+1
+    res = AFGEN2(l,l,x,y,t)
+    return res
+
+	
+
+
+
+
