4ec024c990031f953efb03576d97a419eddd3993
pradal <christophe.pradal@inria.fr>
2019-12-11 21:37:00 +0100

    Update version to 1.1.0 using pkglts

--- src/pycropml/__init__.py
+++ src/pycropml/__init__.py
@@ -1,8 +1,9 @@
 # {# pkglts, base
 
 from . import version
-# {#pkglts,
-__import__('pkg_resources').declare_namespace(__name__)
+
+__version__ = version.__version__
+
 # #}
 __version__ = version.__version__
 
