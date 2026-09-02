bcf14de9e371b670c3a5f8ff1589be5b8ee5ad12
pradal <christophe.pradal@cirad.fr>
2026-09-02 10:12:33 +0200

    Remove dependency to pkg_resources. Fix mix of local and global imports.

--- src/pycropml/interface/__init__.py
+++ src/pycropml/interface/__init__.py
@@ -2,13 +2,9 @@
 
 from . import version
 from IPython.display import display
-from pycropml.interface import design
+from . import design
 display(design.d)
 
 display()
-# {#pkglts,
-__import__('pkg_resources').declare_namespace(__name__)
-# #}
-__version__ = version.__version__
 
-# #}
+__version__ = version.__version__
