9d169a3d06e5a4a1196804a8fd786418f9dc4ce6
cyrillemidingoyi <ahmedmidingoyi@yahoo.fr>
2026-09-05 19:34:09 +0200

    update test new specification template

--- src/pycropml/transpiler/antlr_py/tests/test_to_specification.py
+++ src/pycropml/transpiler/antlr_py/tests/test_to_specification.py
@@ -1,9 +1,7 @@
+from pycropml.transpiler.antlr_py.to_specification import extractMetaInfo
 
-from pycropml.transpiler.antlr_py.to_specification import *
 
-
-specification ="""
-
+FORTRAN_SPECIFICATION = """
 !%%CyML Description Begin%%
 ! ABD      Average bulk density for soil profile (g [soil] / cm3 [soil])
 !            (10.5, [0.5 - 100.0])  state variable
@@ -11,24 +9,53 @@ specification ="""
 !            plant-extractable water (cm) (, [0.0 - 10000]) exogenous variable
 !%%CyML Description End%%
 """
-specification2 = """
+
+PYTHON_ARRAY_SPECIFICATION = """
 #%%CyML Description Begin%%
 # ABD(L)      Average bulk density for soil profile (g [soil] / cm3 [soil])
-#            (10.5, [0.5 - 100.0])  state variable   
+#            (10.5, [0.5 - 100.0])  state variable
 #%%CyML Description End%%
-
 """
 
 
-def test_extractMetaInfo():
-    
-    res1 = extractMetaInfo(specification, "!")
-    print(res1)
-    assert res1 == {"ABD":{"description":"Average bulk density for soil profile","unit":"g [soil] / cm3 [soil]", "default":"10.5",  "max":"100.0", "min":"0.5","len":"", "category":"state", "type":"variable"},\
-                    "TLL" :{"description":"Total soil water in the profile at the lower limit of plant-extractable water", "unit":"cm", "default":"", "min":"0.0", "max": "10000","len":"", "category":"exogenous", "type":"variable"} }
-    res1 = extractMetaInfo(specification2, "#")
-    assert res1 == {"ABD":{"description":"Average bulk density for soil profile","unit":"g [soil] / cm3 [soil]", "default":"10.5",  "max":"100.0", "min":"0.5","len":"L", "category":"state", "type":"variable"}}
+def test_extract_meta_info_from_fortran_comments():
+    assert extractMetaInfo(FORTRAN_SPECIFICATION, "!") == {
+        "ABD": {
+            "description": "Average bulk density for soil profile",
+            "unit": "g [soil] / cm3 [soil]",
+            "default": "10.5",
+            "max": "100.0",
+            "min": "0.5",
+            "len": "",
+            "category": "state",
+            "type": "variable",
+        },
+        "TLL": {
+            "description": (
+                "Total soil water in the profile at the lower limit of "
+                "plant-extractable water"
+            ),
+            "unit": "cm",
+            "default": "",
+            "min": "0.0",
+            "max": "10000",
+            "len": "",
+            "category": "exogenous",
+            "type": "variable",
+        },
+    }
 
 
-test_extractMetaInfo()
-
+def test_extract_meta_info_preserves_array_length():
+    assert extractMetaInfo(PYTHON_ARRAY_SPECIFICATION, "#") == {
+        "ABD": {
+            "description": "Average bulk density for soil profile",
+            "unit": "g [soil] / cm3 [soil]",
+            "default": "10.5",
+            "max": "100.0",
+            "min": "0.5",
+            "len": "L",
+            "category": "state",
+            "type": "variable",
+        }
+    }
